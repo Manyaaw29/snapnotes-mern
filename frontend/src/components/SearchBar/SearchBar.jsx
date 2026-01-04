00e8a69d1482dff495ec9a96db76f59d588c59a1
Manya Awasthi <awasthimanya79@gmail.com>
2026-01-04 16:05:28 +0530

    Search bar functionality enhanced and modified navbar layout a bit

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -12,13 +12,18 @@ const SearchBar = (props) => {
   console.log("onClearSearch:", onClearSearch);
   
   return (
-    <div className='w-40 sm:w-60 md:w-80 flex items-center px-4 bg-slate-100 rounded-md'>
+    <div className='w-40 sm:w-60 md:w-80 flex items-center px-4 bg-slate-100 rounded-md mt-2'>
       <input 
         type="text" 
         placeholder="Search Notes..." 
-        className="w-full text-xs bg-transparent py-3 outline-none" 
+        className="w-full text-xs bg-transparent py-[14px] outline-none" 
         value={value || ""} 
         onChange={onChange}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleSearch();
+          }
+        }}
       />
       {value && (
         <IoMdClose 
