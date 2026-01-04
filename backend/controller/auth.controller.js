45f90648c46cb4cb0b306ed8225e66bd17af4d8e
Manya Awasthi <awasthimanya79@gmail.com>
2026-01-04 15:28:55 +0530

    Fix cookie settings for production

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -48,6 +48,9 @@ export const signin = async (req, res, next) => {
         const { password: pass, ...rest } = validUser._doc;
         res.cookie("access_token", token, {
             httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            maxAge: 24 * 60 * 60 * 1000
         }).status(200).json({ success: true, message: "Login Succesful !", rest });
 
     } catch (error) {
